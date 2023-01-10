47d247146de84db2c2541fc325c96dfd09a88aeb
youngx99 <1286739715@qq.com>
2023-01-10 14:46:31 +0800

    chore:  dev/prd mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -8,38 +8,44 @@ import IconsResolver from 'unplugin-icons/resolver'
 import Pages from 'vite-plugin-pages'
 import { resolve } from 'path'
 
-export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: '@/',
-        replacement: `${resolve(
-          __dirname,
-          'src'
-        )}/`
-      }
-    ]
-  },
-  plugins: [
-    vue(),
-    WindiCSS(),
-    Pages(),
-    AutoImport({
-      imports: ['vue', '@vueuse/core'],
-      dts: true,
-      eslintrc: {
-        enabled: true // disable no-undef rule
-      }
-    }),
-    Components({
-      resolvers: [
-        IconsResolver({
-          prefix: 'i'
+export default defineConfig(
+  ({ command, mode }) => {
+    const config = loadEnv(mode, './')
+    return {
+      base: config.VITE_BASE, // for github pages
+      resolve: {
+        alias: [
+          {
+            find: '@/',
+            replacement: `${resolve(
+              __dirname,
+              'src'
+            )}/`
+          }
+        ]
+      },
+      plugins: [
+        vue(),
+        WindiCSS(),
+        Pages(),
+        AutoImport({
+          imports: ['vue', '@vueuse/core'],
+          dts: true,
+          eslintrc: {
+            enabled: true // disable no-undef rule
+          }
+        }),
+        Components({
+          resolvers: [
+            IconsResolver({
+              prefix: 'i'
+            })
+          ]
+        }),
+        Icons({
+          autoInstall: true
         })
       ]
-    }),
-    Icons({
-      autoInstall: true
-    })
-  ]
-})
+    }
+  }
+)
